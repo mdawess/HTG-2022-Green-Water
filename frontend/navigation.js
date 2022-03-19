17acfa5a410c9805cfc0b1e496ae13a58c98698c
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-03-19 13:08:13 -0400

    added initial screen files

--- frontend/navigation.js
+++ frontend/navigation.js
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import CropInfo from './screens/CropInfo';
+import FarmerStats from './screens/FarmerStats';
+import Home from './screens/Home';
 
-export default function navigation() {
-  return (
-    <View>
-      <Text>navigation</Text>
-    </View>
-  )
+export default function RootNavigation() {
+    const Stack = createStackNavigator();
+
+    const screenOptions = {
+        headerShown: false
+    }
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="CropInfo" screenOptions={screenOptions} >
+                {/* <Stack.Screen name='CropInfo' component={CropInfo} />
+                <Stack.Screen name='Home' component={Home} />
+                <Stack.Screen name='FarmerStats' component={FarmerStats} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
