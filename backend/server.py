5f22876f986dfaed8feb508bc613f1055198acd7
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2022-03-19 10:54:06 -0400

    Added intial server

--- backend/server.py
+++ backend/server.py
@@ -3,3 +3,14 @@ from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
+
+
+@app.route('/<string:data_path>', methods=['GET'])
+def get_data_from_nyanda(data_path):
+    """
+    Pull the data from the data visualization files    
+    """
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.233', port=5000, debug=True)
